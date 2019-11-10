cd7bd8e65cec0d2df369e6ac23eb88ef469a4f13
KAMIL0024 <kamilbydpol@fratikbot.pl>
2019-11-10 19:58:23 +0100

    Byc moze dziala

--- liczek/src/main/java/pl/fratik/liczek/LiczekListener.java
+++ liczek/src/main/java/pl/fratik/liczek/LiczekListener.java
@@ -19,15 +19,15 @@ package pl.fratik.liczek;
 
 import com.google.common.eventbus.Subscribe;
 import lombok.Getter;
-import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.*;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import org.apache.commons.lang.math.NumberUtils;
+import org.codehaus.stax2.ri.typed.NumberUtil;
+import org.jsoup.helper.StringUtil;
 import pl.fratik.core.entity.GuildConfig;
 import pl.fratik.core.entity.GuildDao;
 import pl.fratik.core.tlumaczenia.Language;
 import pl.fratik.core.tlumaczenia.Tlumaczenia;
-
-import java.util.List;
 
 public class LiczekListener {
     private final GuildDao guildDao;
@@ -41,17 +41,46 @@ public class LiczekListener {
     @Subscribe
     public void onGuildMessageReceivedEvent(GuildMessageReceivedEvent e) {
         GuildConfig gc = guildDao.get(e.getGuild());
-        if (e.getChannel().getType() != ChannelType.TEXT) return;
-        String id = gc.getLiczekKanal();
-        if (id != null || !id.equals("0")) {
-            TextChannel txt = e.getGuild().getTextChannelById(id);
-            if (txt != null) {
-                if (e.getMember().getUser().isFake() || e.getMember().getUser().isBot()) {
-                    e.getMessage().delete().queue();
-                    return;
-                }
-                String[] msg = String.valueOf(e.getMessage()).split(" ");
-                e.getChannel().sendMessage("pierwsze zdanie to" + msg[0]);
+        if (e.getChannel().getType() != ChannelType.TEXT) { return; }
+        if (gc.getLiczekKanal().equals(e.getChannel())) {
+            if (e.getMember().getUser().isFake() || e.getMessage().getEmbeds().get(0) != null) {
+                e.getMessage().delete().queue();
+                return;
             }
-        }}
+
+            String[] msg = String.valueOf(e.getMessage()).split(" ");
+            if (msg[0].isEmpty() || !StringUtil.isNumeric(msg[0])) {
+                e.getMessage().delete().queue();
+                return;
+            }
+            Integer wyslanaLiczba = Integer.parseInt(msg[0]);
+            if (wyslanaLiczba != gc.getLiczekLiczba()+1 || e.getMember().equals(gc.getLiczekOstatniaOsoba())) {
+                e.getMessage().delete().queue();
+                return;
+            }
+
+            gc.setLiczekLiczba(wyslanaLiczba);
+            gc.setLiczekOstatniaOsoba(e.getMember().getUser().getId());
+            guildDao.save(gc);
+            refreshTopic(e.getChannel());
+        }
     }
+
+    public void refreshTopic(TextChannel cha) {
+        GuildConfig gc = guildDao.get(cha.getGuild());
+
+        Integer liczba = gc.getLiczekLiczba()+1;
+        User osoba = cha.getJDA().getUserById(gc.getLiczekLiczba());
+
+        if (liczba == null || osoba == null) return;
+
+        Language l = tlumaczenia.getLanguage(cha.getGuild());
+        String msg = tlumaczenia.get(l, "liczek.topic", osoba.getAsMention(), liczba);
+
+        try {
+            cha.getManager().setTopic(msg);
+        } catch (Exception xd) {
+            /* brak permów, idziem sobie*/
+        }
+    }
+}
