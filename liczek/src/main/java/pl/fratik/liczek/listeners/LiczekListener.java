85d1e42d95805496b4f5577a8ba5246beb10f8a7
KAMIL0024 <kamilbydpol@fratikbot.pl>
2019-12-05 21:56:01 +0100

    Dyskwalifikacja w liczku

--- liczek/src/main/java/pl/fratik/liczek/listeners/LiczekListener.java
+++ liczek/src/main/java/pl/fratik/liczek/listeners/LiczekListener.java
@@ -18,16 +18,27 @@
 package pl.fratik.liczek.listeners;
 
 import com.google.common.eventbus.Subscribe;
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.audit.ActionType;
+import net.dv8tion.jda.api.audit.AuditLogEntry;
+import net.dv8tion.jda.api.audit.AuditLogOption;
 import net.dv8tion.jda.api.entities.ChannelType;
+import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.events.message.MessageUpdateEvent;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageDeleteEvent;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.api.exceptions.PermissionException;
 import pl.fratik.core.entity.GuildConfig;
 import pl.fratik.core.entity.GuildDao;
 import pl.fratik.core.tlumaczenia.Language;
 import pl.fratik.core.tlumaczenia.Tlumaczenia;
+import pl.fratik.core.util.UserUtil;
 import pl.fratik.liczek.entity.Liczek;
 import pl.fratik.liczek.entity.LiczekDao;
+
+import java.time.OffsetDateTime;
 
 public class LiczekListener {
     private final GuildDao guildDao;
@@ -58,7 +69,7 @@ public class LiczekListener {
 
             try {
                 wyslanaLiczba = Integer.parseInt(kek[0]);
-            } catch(NumberFormatException xd) {
+            } catch (NumberFormatException xd) {
                 e.getMessage().delete().queue();
                 return;
             }
@@ -74,6 +85,45 @@ public class LiczekListener {
             refreshTopic(e.getChannel(), e.getAuthor());
             liczekDao.save(licz);
         }
+    }
+
+    @Subscribe
+    public void onGuildMessageDeleteEvent(GuildMessageDeleteEvent e) {
+        GuildConfig gc = guildDao.get(e.getGuild());
+        if (gc.getLiczekKanal() == null) return;
+        if (gc.getLiczekKanal().isEmpty()) return;
+        if (!e.getChannel().getId().equals(gc.getLiczekKanal())) return;
+        try {
+            e.getGuild().retrieveAuditLogs().type(ActionType.MESSAGE_DELETE).queue(
+                    audiologi -> {
+                        User deletedBy = null;
+                        for (AuditLogEntry log : audiologi) {
+                            if (log.getType() == ActionType.MESSAGE_DELETE
+                                    && log.getTimeCreated().isAfter(OffsetDateTime.now().minusMinutes(1))
+                                    && e.getChannel().getId().equals(log.getOption(AuditLogOption.CHANNEL))) {
+                                deletedBy = log.getUser();
+                                break;
+                            }
+                        }
+                        if (deletedBy != null) dyskwalifikacja(deletedBy, e.getChannel());
+                    }
+
+            );
+        } catch (Exception kek) {
+            /* lul */
+        }
+    }
+
+    @Subscribe
+    public void onMessageUpdateEvent(MessageUpdateEvent e) {
+        GuildConfig gc = guildDao.get(e.getGuild());
+        if (gc.getLiczekKanal() == null) return;
+        if (gc.getLiczekKanal().isEmpty()) return;
+        if (!e.getChannel().getId().equals(gc.getLiczekKanal())) return;
+        if (e.getChannel().getType() != ChannelType.TEXT) return;
+        TextChannel ok = e.getGuild().getTextChannelById(e.getChannel().getId());
+        if (ok == null) return;
+        dyskwalifikacja(e.getAuthor(), ok);
     }
 
     private void refreshTopic(TextChannel cha, User osoba) {
@@ -92,4 +142,26 @@ public class LiczekListener {
             /* brak permów, idziem sobie*/
         }
     }
+
+    private void dyskwalifikacja(User user, TextChannel cha) {
+        Member mem = cha.getGuild().getMemberById(user.getId());
+        if (mem == null) return;
+        GuildConfig gc = guildDao.get(mem.getGuild());
+        Language l = tlumaczenia.getLanguage(mem.getGuild());
+        String msg = tlumaczenia.get(l, "liczek.dyskwalifikacja", mem.getAsMention(), UserUtil.formatDiscrim(mem), user.getId());
+
+        try {
+            cha.putPermissionOverride(mem).deny(Permission.MESSAGE_ADD_REACTION, Permission.MESSAGE_WRITE).complete();
+        } catch (PermissionException xd) {
+            /* lul */
+        }
+
+        if (gc.getKanalAdministracji() != null && !gc.getKanalAdministracji().isEmpty()) {
+            TextChannel adm = mem.getGuild().getTextChannelById(gc.getKanalAdministracji());
+            if (adm != null) {
+                if (adm.canTalk()) adm.sendMessage(msg).queue();
+            }
+        }
+    }
+
 }
