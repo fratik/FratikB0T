d8e80032ee5116596604c15500c12d73f2aa4854
Mateusz Ładyko <fratik@fratikbot.pl>
2020-08-28 23:12:47 +0200

    poprawki

--- music/src/main/java/pl/fratik/music/managers/SearchManager.java
+++ music/src/main/java/pl/fratik/music/managers/SearchManager.java
@@ -31,8 +31,6 @@ import pl.fratik.core.util.NetworkUtil;
 import pl.fratik.core.util.TimeUtil;
 
 import java.net.URLEncoder;
-import java.time.Duration;
-import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -41,7 +39,6 @@ public class SearchManager {
     private final Logger logger = LoggerFactory.getLogger(SearchManager.class);
     private final String ytApiUrl;
     private final String ytApiUrlThumbnails;
-    private final String ytApiUrlThumbnailsAndDuration;
     private final NowyManagerMuzyki managerMuzyki;
     private final Cache<SearchResult> youtubeResults;
     private final Cache<SearchResult.SearchEntry> entryCache;
@@ -49,7 +46,6 @@ public class SearchManager {
     public SearchManager(String ytApiKey, String ytApiKeyThumbnails, NowyManagerMuzyki managerMuzyki, RedisCacheManager redisCacheManager) {
         ytApiUrl = "https://www.googleapis.com/youtube/v3/search?part=snippet&key=" + ytApiKey + "&maxResults=20&q=";
         ytApiUrlThumbnails = "https://www.googleapis.com/youtube/v3/videos?part=snippet&key=" + ytApiKeyThumbnails + "&id=";
-        ytApiUrlThumbnailsAndDuration = "https://www.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails&key=" + ytApiKeyThumbnails + "&id=";
         this.managerMuzyki = managerMuzyki;
         youtubeResults = redisCacheManager.new CacheRetriever<SearchResult>(){}.getCache((int) TimeUnit.MINUTES.toSeconds(30));
         entryCache = redisCacheManager.new CacheRetriever<SearchResult.SearchEntry>(){}.getCache((int) TimeUnit.HOURS.toSeconds(1));
@@ -142,40 +138,6 @@ public class SearchManager {
 
     }
 
-    public SearchResult.SearchEntry getThumbnailAndDuration(String videoId) {
-        SearchResult.SearchEntry kupa = entryCache.getIfPresent(videoId);
-        if (kupa != null && kupa.hasDuration() && kupa.getThumbnailURL() != null) return kupa;
-        try {
-            String data = new String(NetworkUtil.download(ytApiUrlThumbnailsAndDuration + videoId), Charsets.UTF_8);
-            JsonObject element2 = GsonUtil.GSON.fromJson(data, JsonObject.class);
-            JsonObject thumbnails = element2.get("items").getAsJsonArray().get(0).getAsJsonObject()
-                    .get("snippet").getAsJsonObject().get("thumbnails").getAsJsonObject();
-            JsonObject maxResThumbnail = null;
-            for (String key : thumbnails.keySet()) {
-                if (maxResThumbnail == null) {
-                    maxResThumbnail = thumbnails.get(key).getAsJsonObject();
-                    continue;
-                }
-                if (thumbnails.get(key).getAsInt() > maxResThumbnail.get("height").getAsInt())
-                    maxResThumbnail = thumbnails.get(key).getAsJsonObject();
-            }
-            if (maxResThumbnail == null) throw new IllegalStateException("brak miniatur");
-            SearchResult obj = new SearchResult();
-            obj.addEntry(element2.get("items").getAsJsonArray().get(0).getAsJsonObject().get("snippet").getAsJsonObject()
-                            .get("title").getAsString(), "https://youtube.com/watch?v=" + videoId,
-                    TimeUnit.MILLISECONDS.convert(Duration.parse(element2.get("items").getAsJsonArray()
-                            .get(0).getAsJsonObject().get("contentDetails").getAsJsonObject().get("duration")
-                            .getAsString()).get(ChronoUnit.SECONDS), TimeUnit.SECONDS),
-                    maxResThumbnail.getAsJsonObject().get("url").getAsString());
-            SearchResult.SearchEntry odp = obj.entries.get(0);
-            entryCache.put(videoId, odp);
-            return odp;
-        } catch (Exception ignored) {
-            /*lul*/
-        }
-        return null;
-    }
-
     public static class SearchResult {
         @Getter private final List<SearchEntry> entries = new ArrayList<>();
 
@@ -203,10 +165,6 @@ public class SearchManager {
 
             public long getDurationAsInt() {
                 return duration;
-            }
-
-            boolean hasDuration() {
-                return duration != 0;
             }
         }
     }
