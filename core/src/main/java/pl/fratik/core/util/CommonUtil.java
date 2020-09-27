eab7624252c184d4fdda1195ed0edfd93730a6b3
Mateusz Ładyko <fratik@fratikbot.pl>
2020-09-27 13:30:38 +0200

    find, a nie matches

--- core/src/main/java/pl/fratik/core/util/CommonUtil.java
+++ core/src/main/java/pl/fratik/core/util/CommonUtil.java
@@ -183,7 +183,7 @@ public class CommonUtil {
     public static String getImageUrl(Message msg) {
         Matcher matcher = Pattern.compile("(http(s)?):\\/\\/(www\\.)?[?a-zA-Z0-9@:%._\\+~#=]{2,256}\\.[a-z]{2,6}" +
                 "\\b([-a-zA-Z0-9@:%_\\+.~#?&//=]*.png)", Pattern.CASE_INSENSITIVE | Pattern.DOTALL).matcher(msg.getContentRaw());
-        if (matcher.matches()) return matcher.group();
+        if (matcher.find()) return matcher.group();
         if (!msg.getAttachments().isEmpty() && msg.getAttachments().get(0).isImage())
             return msg.getAttachments().get(0).getUrl();
         return null;
