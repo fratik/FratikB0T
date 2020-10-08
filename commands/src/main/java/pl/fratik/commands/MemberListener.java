d8be2c632b19c6a66eafa4bfd838d3af78caa371
Mateusz Ładyko <fratik@fratikbot.pl>
2020-10-08 22:46:38 +0200

    jFB0T-134 tak

--- commands/src/main/java/pl/fratik/commands/MemberListener.java
+++ commands/src/main/java/pl/fratik/commands/MemberListener.java
@@ -193,7 +193,7 @@ class MemberListener {
         for (Role role : mem.getRoles().stream().sorted(Comparator.reverseOrder()).collect(Collectors.toList())) {
             prefix = gc.getRolePrefix().get(role.getId());
         }
-        if (prefix != null) nick += prefix + " " + nick;
+        if (prefix != null) nick = prefix + " " + nick;
         try {
             if (nick.length() > 28) nick = nick.substring(0, 28) + "...";
             mem.getGuild().modifyNickname(mem, nick).queue();
