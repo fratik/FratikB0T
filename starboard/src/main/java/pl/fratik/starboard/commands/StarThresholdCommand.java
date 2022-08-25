23dfb3608edc3bd2c9751115012d577ca5ef9728
KamelaJda <kamilbydpol@gmail.com>
2022-08-25 15:44:50 +0200

    StarThreshold dla MANAGE_SERVER

--- starboard/src/main/java/pl/fratik/starboard/commands/StarThresholdCommand.java
+++ starboard/src/main/java/pl/fratik/starboard/commands/StarThresholdCommand.java
@@ -19,7 +19,6 @@ package pl.fratik.starboard.commands;
 
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.interactions.commands.DefaultMemberPermissions;
-import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 import org.jetbrains.annotations.NotNull;
 import pl.fratik.core.command.NewCommand;
 import pl.fratik.core.command.NewCommandContext;
@@ -33,7 +32,7 @@ public class StarThresholdCommand extends NewCommand {
     public StarThresholdCommand(StarDataDao starDataDao) {
         this.starDataDao = starDataDao;
         name = "starthreshold";
-        permissions = DefaultMemberPermissions.enabledFor(Permission.ADMINISTRATOR);
+        permissions = DefaultMemberPermissions.enabledFor(Permission.MANAGE_SERVER);
         usage = "<ilosc:integer>";
     }
 
